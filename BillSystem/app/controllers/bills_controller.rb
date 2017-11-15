7059e31cd7b701da6c3243757a0feb7568a67a15
DmytroKashchuk <dmytro.kashchuk@studenti.unitn.it>
2017-11-15 21:44:31 +0100

    adding new and create method for bills

--- BillSystem/app/controllers/bills_controller.rb
+++ BillSystem/app/controllers/bills_controller.rb
@@ -1,4 +1,26 @@
 class BillsController < ApplicationController
   def new
+  	@client = Client.find params[:client_id]
+  	@bill = Bill.new
   end
+
+  def create
+    @client = Client.find params[:client_id] # we get the incident from the route
+    # we build the status update using a method of the relation
+    @bill = @client.bills.build(bill_params)
+    respond_to do |format|
+      if @bill.save
+        format.html { redirect_to @client, notice: 'bill was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+ 
+  private
+ 
+  def bill_params
+    params.require(:bill).permit(:note)
+  end
+
 end
--- BillSystem/config/routes.rb
+++ BillSystem/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'bills/new'
 
-  resources :clients
+  resources :clients do
+  	resources :bills, only: [:new, :create, :destroy]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
