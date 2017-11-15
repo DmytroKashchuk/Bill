79831f5e425d84c8a5040b467e826e0fd4705da8
DmytroKashchuk <dmytro.kashchuk@studenti.unitn.it>
2017-11-15 20:44:28 +0100

    Controll of bill presences

--- BillSystem/app/models/client.rb
+++ BillSystem/app/models/client.rb
@@ -1,7 +1,12 @@
 class Client < ApplicationRecord
 
+	has_many :bills
+
 	validates :name, presence: true
 	validates :surename, presence: true
 	validates :fiscalCode, presence: true
 	validates :hourTariff, presence: true
+
+
+
 end
